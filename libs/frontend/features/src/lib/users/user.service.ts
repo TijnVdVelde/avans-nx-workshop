966138b3b08f45af261561490114c48386910251
Tijn Van de Velde <70897975+TijnVdVelde@users.noreply.github.com>
2024-11-19 23:32:49 +0100

    + styling toegevoegd
    zo krijg je gelijk meer een gevoel voor de webapp. header en footer zijn ook mooi meegenomen.

--- libs/frontend/features/src/lib/users/user.service.ts
+++ libs/frontend/features/src/lib/users/user.service.ts
@@ -1,52 +1,53 @@
-import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
-import { IUserInfo, UserRole, UserGender } from '@avans-nx-workshop/shared/api';
+import { Injectable } from "@angular/core";
+import { delay, Observable, of } from "rxjs";
+import { IUserInfo, UserRole, UserGender } from "@avans-nx-workshop/shared/api";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root"
 })
 export class UserService {
     readonly users: IUserInfo[] = [
         {
-            _id: '1',
-            name: 'Tijn Van de Velde',
-            emailAddress: 'tmh.vandevelde@studennt.avans.nl',
+            _id: "1",
+            name: "Tijn Van de Velde",
+            emailAddress: "tmh.vandevelde@student.avans.nl",
             role: UserRole.Unknown,
             gender: UserGender.Male,
-            password: 'secret',
+            password: "secret",
             isActive: true,
-            profileImgUrl: 'https://randomuser.me/api/portraits/men/1.jpg'
+            profileImgUrl: "https://randomuser.me/api/portraits/men/1.jpg"
         },
         {
-            _id: '2',
-            name: 'Bibi Van de Velde',
-            emailAddress: 'blm.vandevelde@student.avans.nl',
+            _id: "2",
+            name: "Max Verstappen",
+            emailAddress: "unknown",
             role: UserRole.Unknown,
-            gender: UserGender.Female,
-            password: 'secret',
+            gender: UserGender.Male,
+            password: "secret",
             isActive: true,
-            profileImgUrl: 'https://randomuser.me/api/portraits/women/66.jpg'
-        },
+            profileImgUrl:
+                "https://www.formula1.com/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/1col/image.png"
+        }
     ];
 
     constructor() {
-        console.log('UserService created');
+        console.log("UserService created");
     }
 
     getUsers(): IUserInfo[] {
-        console.log('getUsers() aangeroepen');
+        console.log("getUsers() aangeroepen");
         return this.users;
     }
 
     getUsersAsObservable(): Observable<IUserInfo[]> {
-        console.log('getUsersAsObservable() aangeroepen');
+        console.log("getUsersAsObservable() aangeroepen");
         // 'of' is een rxjs operator die een Observable
         // maakt van de gegeven data.
         return of(this.users).pipe(delay(2000));
     }
 
     getUserById(id: string | null): IUserInfo {
-        console.log('getUserById() aangeroepen');
+        console.log("getUserById() aangeroepen");
         return this.users.filter((user) => user._id === id)[0];
     }
 }
