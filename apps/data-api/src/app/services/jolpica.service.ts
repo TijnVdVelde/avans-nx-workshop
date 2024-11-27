84c15a915e25c0ad301376066a0a8fecbd44d414
Tijn Van de Velde <70897975+TijnVdVelde@users.noreply.github.com>
2024-11-27 23:37:33 +0100

    * api optimalistatie
    nu pakt die het nieuwe domein van de api die langer doorgaat dan 2024

--- apps/data-api/src/app/services/jolpica.service.ts
+++ apps/data-api/src/app/services/jolpica.service.ts
@@ -3,27 +3,16 @@ import axios from 'axios';
 
 @Injectable()
 export class JolpicaService {
-    private readonly baseUrl = 'http://ergast.com/ergast/f1';
+    private readonly baseUrl = 'https://api.jolpi.ca/ergast/f1';
 
     async fetchDrivers() {
         try {
-            const response = await axios.get('https://ergast.com/api/f1/2024/drivers.json');
+            const response = await axios.get(`${this.baseUrl}/2024/drivers.json`);
             console.log('Fetched drivers:', response.data); // Debugging log
             return response.data.MRData.DriverTable.Drivers; // Check API structure
         } catch (error) {
             console.error('Error fetching drivers:', error.message);
             throw new Error('Failed to fetch drivers from Ergast API');
-        }
-    }
-
-    async fetchRaces() {
-        try {
-            const response = await axios.get('https://ergast.com/api/f1/2024.json');
-            console.log('Fetched races:', response.data); // Debugging log
-            return response.data.MRData.RaceTable.Races; // Check API structure
-        } catch (error) {
-            console.error('Error fetching races:', error.message);
-            throw new Error('Failed to fetch races from Ergast API');
         }
     }
 }
